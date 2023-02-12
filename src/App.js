06417bb9accb5b4787f7893ecf094728ac8362fa
Jwalin <jwalin.maniar@colorado.edu>
2023-02-13 04:10:36 +0530

    First commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div id='container'>
+      <div id='main'>
+        <span>"Hello world"</span>
+        {/* <Switch>
+          <Route path='/' component={Main} />
+        </Switch> */}
+      </div>
+
+      <div id='footer'>
+        {/* <Footer /> */}
+      </div>
+    </div>
+  );
+}
+
+export default App;
